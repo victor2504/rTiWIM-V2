13f454ed2061f69b57a885f4d42f4268ec298b2d
victor2504 <70090848+victor2504@users.noreply.github.com>
2023-02-12 19:23:38 -0300

    Update conexaobd.php

--- conexaobd.php
+++ conexaobd.php
@@ -1,11 +1,6 @@
 <?php
 
-//Conexao Hostinger
-//$localhost = "localhost";
-//$user = "u386477866_rtiwim";
-//$pass = "Jv250491!";
-//$database = "u386477866_rtiwim";
-//$conexao = mysqli_connect($localhost,$user,$pass,$database) or die ('Erro na consulta!'); 
+
 
 
 //Conexao Local
